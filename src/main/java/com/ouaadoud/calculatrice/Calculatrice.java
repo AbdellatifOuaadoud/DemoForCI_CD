807509f19996083f1089bfa46532edad0e874f2d
AbdellatifOuaadoud <108677871+AbdellatifOuaadoud@users.noreply.github.com>
2023-12-21 13:43:49 -0500

    last commit

---
+++ src/main/java/com/ouaadoud/calculatrice/Calculatrice.java
@@ -0,0 +1,37 @@
+package com.ouaadoud.calculatrice;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+
+@WebService(serviceName = "CalculatriceOuaadoud")
+public class Calculatrice {
+
+    @WebMethod
+    public int add (int a, int b) {
+        return a + b;
+    }
+    @WebMethod
+    public double minus (double a, double b) {
+        return a - b;
+    }
+    @WebMethod
+    public double multiply (double a, double b) {
+        return a * b;
+    }
+    @WebMethod
+    public String divide (double a, double b) {
+        try {
+            double result = a / b;
+            if (Double.isInfinite(result)) {
+                return "Erreur: Division par Zéro!";
+            }
+            return Double.toString(result);
+        } catch (ArithmeticException e) {
+            return "Erreur: Division par Zéro!";
+        }
+    }
+    @WebMethod
+    public double power (double a, double b) {
+        return Math.pow(a, b);
+    }
+}
